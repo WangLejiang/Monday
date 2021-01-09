a3f02edaa90bc734438a499a95c86a4a1584e6f1
wanglejiang <wl.j@outlook.com>
2021-01-09 23:58:50 +0800

    AuthServiceApplication

---
+++ monday-auth/auth-service/src/main/java/com/wanglejiang/auth/AuthServiceApplication.java
@@ -0,0 +1,8 @@
+package com.wanglejiang.auth;
+
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
