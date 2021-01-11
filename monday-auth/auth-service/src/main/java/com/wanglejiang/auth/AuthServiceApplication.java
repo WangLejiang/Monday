47c578e39037dd1e1445e14d40e209538dcac9e6
wanglejiang <wl.j@outlook.com>
2021-01-11 23:59:03 +0800

    dependency change

--- monday-auth/auth-service/src/main/java/com/wanglejiang/auth/AuthServiceApplication.java
+++ monday-auth/auth-service/src/main/java/com/wanglejiang/auth/AuthServiceApplication.java
@@ -1,5 +1,7 @@
 package com.wanglejiang.auth;
 
+import org.springframework.boot.SpringApplication;
+
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
